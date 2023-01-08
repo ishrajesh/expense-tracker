49f29846a461e5827d9d7cfc96d589bdcf34af8b
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-08 10:06:26 +0530

    testing user name / email for commits

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,6 +5,6 @@ const initialState = {
         {id: 1, text: "Flower", amount: -50},
         {id: 2, text: "Salary", amount: +1500},
         {id: 3, text: "Drinks", amount: -30},
-        {id: 4, text: "Camera", amount: +300},
+        {id: 4, text: "Camera", amount: +300} ,
     ]
 }
